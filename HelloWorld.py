f578933b29a3ccacd700bd5ac292959e9948297f
Alan Abraham <47579004+Alantrivandrum@users.noreply.github.com>
2020-01-24 02:37:42 +0530

    Add files via upload

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,3 +5,8 @@ print("Hello " + name + ", nice to meet you")
 age = 2020 - int(year)
 
 print("You are " + str(age) + " years old")
+
+if(age < 18):
+    print("You are under 18")
+else:
+    print("You are an adult")

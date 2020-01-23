3c39dc63c1f54ebb722509cb4ce8d8999bc658b5
Alan Abraham <47579004+Alantrivandrum@users.noreply.github.com>
2020-01-24 02:32:49 +0530

    Added my first python file

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+name = input("What is your name")
+year = input("What year were your born ? ")
+
+print("Hello " + name + ", nice to meet you")
+age = 2020 - int(year)
+
+print("You are " + str(age) + " years old")
